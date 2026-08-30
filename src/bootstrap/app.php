1ba5e65483d057050e5beeed67d0530dc0731456
Tiago Nogueira <tiagonogueirajf@gmail.com>
2026-08-30 03:46:44 +0000

    config produção: remove porta pública mysql, fix cache npm, trust proxies https

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -16,7 +16,14 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
+	$middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_HOST |
+                     Request::HEADER_X_FORWARDED_PORT |
+                     Request::HEADER_X_FORWARDED_PROTO |
+                     Request::HEADER_X_FORWARDED_AWS_ELB
+        );
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
