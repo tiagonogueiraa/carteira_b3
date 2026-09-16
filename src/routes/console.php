e7282c6b4b3635f07508f8a394520bb49250fccb
Tiago <seu_email@exemplo.com>
2026-09-16 12:03:15 -0300

    fix: adicionado use no console para rodar o cron

--- src/routes/console.php
+++ src/routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use App\Services\YahooFinanceService;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
