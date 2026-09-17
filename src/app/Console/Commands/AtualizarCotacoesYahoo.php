ea462bd0dd760bca972cfb9b484dbd0f1b613743
Tiago <seu_email@exemplo.com>
2026-09-17 13:41:29 -0300

    fix: ajuste atualização preço

--- src/app/Console/Commands/AtualizarCotacoesYahoo.php
+++ src/app/Console/Commands/AtualizarCotacoesYahoo.php
@@ -29,6 +29,24 @@ class AtualizarCotacoesYahoo extends Command
                 'response' => $dados,
             ]);
 
+            if ($dados) {
+                $stock = Stock::where('ticker', $ticker)->first();
+
+                StockMarket::create([
+                    'stock_id' => $stock->id,
+                    'short_name' => $dados['shortName'] ?? null,
+                    'long_name' => $dados['longName'] ?? null,
+                    'currency' => $dados['currency'] ?? null,
+                    'regular_market_price' => $dados['regularMarketPrice'] ?? null,
+                    'regular_market_change' => $dados['regularMarketChange'] ?? null,
+                    'regular_market_change_percent' => $dados['regularMarketChangePercent'] ?? null,
+                    'regular_market_day_high' => $dados['regularMarketDayHigh'] ?? null,
+                    'regular_market_day_low' => $dados['regularMarketDayLow'] ?? null,
+                    'regular_market_volume' => $dados['regularMarketVolume'] ?? null,
+                    'regular_market_time' => $dados['regularMarketTime'] ?? null,
+                    'market_cap' => $dados['marketCap'] ?? null,
+                ]);
+            }
             sleep(rand(2, 5)); // espaçamento entre requisições
 
             $this->info("{$ticker}: " . ($dados ? 'atualizado' : 'falhou'));
